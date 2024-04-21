2a1f3f23a3b1789c8085c4044a7ea342e6d09f86
Amin <119117189+am1nNP@users.noreply.github.com>
2024-04-21 21:49:57 +0330

    upload image fixed

--- src/empInsertAction.php
+++ src/empInsertAction.php
@@ -9,9 +9,25 @@ if (isset($_POST['submit'])) {
         $gender = $conn->escape_string($_POST["gender"]);
         $phone = $conn->escape_string($_POST["phone"]);
         $image = $conn->escape_string($_POST["image"]);
-        $targetImage="uploads/".$image;          
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["image"]["tmp_name"]);
+            if($check !== false) {
+                move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+                echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
         $query = 'INSERT INTO emp(name,age,gender,phone,image)';
-        $query .= "VALUES('$name', '$age','$gender','$phone','$targetImage')";
+        $query .= "VALUES('$name', '$age','$gender','$phone','$target_file')";
     if($conn->query($query)){
         if (mysqli_affected_rows($conn)) {
             $_SESSION["empInsert"]="true";
